e7d6224eed69ea7035d357f42b63212a3dbe75ab
dbrightman <danieljbrightman@gmail.com>
2026-04-01 21:30:01 -0400

    4/1: placeholder text

--- src/placeholder.cpp
+++ src/placeholder.cpp
@@ -1,1 +1,13 @@
-// Work will be added when Joel and I find a time when we are able to meet
+/* 
+Work will be added when Joel and I find a time when we are able to meet
+
+I have not had time to work on the actual code and implementation for the 60% completion
+section because the assignment was posted on the 26th and I had a midterm on the 1st,
+as well as other assignments due in between.
+Joel has had a busy week with his startup and with other work as well.
+
+We intend to meet this weekend or next week on a day that works for both of us
+to work through a large chunk of the final project
+
+Thank you for your understanding.
+*/
